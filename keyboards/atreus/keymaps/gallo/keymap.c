de2a71feea9a44481a0722bff9bfcebb7065a49d
Alejandro Gallo <aamsgallo@gmail.com>
2019-10-26 02:15:41 +0200

    Customize the hell out of this

--- keyboards/atreus/keymaps/gallo/keymap.c
+++ keyboards/atreus/keymaps/gallo/keymap.c
@@ -4,13 +4,10 @@
 #include QMK_KEYBOARD_H
 #include "config.h"
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define BASE 0
-#define _RS 1
-#define _LW 2
+#define PROGRAMMING_LAYER 1
+#define MOVEMENT_LAYER 2
+#define LAYER_LAYER 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -28,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_BSPC,  KC_LSFT, KC_LCTL, KC_LALT, KC_SPC, MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    KC_ESC, KC_TAB,  TO(LAYER_LAYER),  KC_BSPC,  KC_LSFT, KC_LCTL, KC_LALT, KC_SPC, MO(PROGRAMMING_LAYER), KC_MINS, KC_QUOT, KC_ENT
   ),
 
 /*
@@ -42,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     |    |    |    |    |    |    |    |    |    |    | '  |    |    |
     +----+----+----+----+----+----+    +----+----+----+----+----+----+
 */
-  [_RS] = GLAYOUT( /* [> RAISE <] */
+  [PROGRAMMING_LAYER] = GLAYOUT( /* [> RAISE <] */
     KC_EXLM, KC_AT,   KC_HASH, KC_DOLLAR, KC_PERCENT,              KC_CIRCUMFLEX, KC_7,    KC_8,   KC_9, KC_ASTR ,
     KC_LCBR, KC_RCBR, KC_QUOT, KC_GRAVE, KC_PIPE,                  KC_BSPC, KC_3,    KC_5,   KC_6, KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_0,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-    TG(_LW), KC_INS,  KC_LGUI, KC_BSPC, KC_LSFT, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
+    KC_ESC, KC_INS,  KC_LGUI, KC_BSPC, KC_LSFT, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, TG(MOVEMENT_LAYER), KC_EQL  ),
 
 /*
     +----+----+----+----+----+              +----+----+----+----+----+
@@ -59,9 +56,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     |    |    |    |    |    |    |    |    |    |    | '  |    |    |
     +----+----+----+----+----+----+    +----+----+----+----+----+----+
 */
-  [_LW] = GLAYOUT( /* [> LOWER <] */
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_BSPC, KC_LSFT, KC_LCTL, KC_LALT, KC_SPC,  TO(BASE), KC_PSCR, KC_SLCK, KC_PAUS )
+  [MOVEMENT_LAYER] = GLAYOUT( /* [> LOWER <] */
+    KC_VOLU,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_MS_WH_UP,   KC_MS_BTN1,   KC_MS_U,        KC_MS_BTN2,   KC_DEL,
+    KC_VOLD,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_MS_WH_DOWN, KC_MS_L,      KC_MS_D,        KC_MS_R,   KC_INS,
+    KC_NO,   KC_NO, KC_NO,   KC_NO,   RESET,                     KC_MS_WH_LEFT, KC_MS_WH_RIGHT,   KC_MS_BTN3, KC_F3,   KC_F12  ,
+    TO(BASE),   KC_NO, KC_LGUI, KC_BSPC, KC_LSFT, KC_LCTL, KC_LALT, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO ),
+
+  [LAYER_LAYER] = GLAYOUT(
+    KC_NO,  KC_NO, KC_NO,   KC_NO,  KC_NO,                   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_NO,
+    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO,      KC_NO,        KC_NO,   KC_NO,
+    KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,                     KC_NO, KC_NO,   KC_NO, KC_NO,   KC_NO,
+    TO(BASE), KC_NO, TO(MOVEMENT_LAYER), KC_NO, KC_NO, KC_LCTL, KC_LALT, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO ),
 };
