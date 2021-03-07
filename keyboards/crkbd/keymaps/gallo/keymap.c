60d66743844a4ec40ad31b7285b1ebaad13ff773
Alejandro Gallo <aamsgallo@gmail.com>
2021-03-07 21:02:50 +0100

    Delete most unnecessary stuff

--- keyboards/crkbd/keymaps/gallo/keymap.c
+++ keyboards/crkbd/keymaps/gallo/keymap.c
@@ -1,24 +1,10 @@
 #include QMK_KEYBOARD_H
-#ifdef PROTOCOL_LUFA
-  #include "lufa.h"
-  #include "split_util.h"
-#endif
-#ifdef SSD1306OLED
-  #include "ssd1306.h"
-#endif
+
+#ifdef GALLO_JOYSTICK
 #include "pointing_device.h"
-#ifdef GALLO_JOYSTICK
 #include "joystick.h"
-#endif
 
-extern keymap_config_t keymap_config;
 extern uint8_t is_master;
-#ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
-
-#ifdef GALLO_JOYSTICK
 joystick_config_t joystick_axes[JOYSTICK_AXES_COUNT] = {
     [0] = JOYSTICK_AXIS_IN(B4, 0, 511, 1023), // 10 bit adc
     [1] = JOYSTICK_AXIS_IN(B5, 0, 504, 1023), // 10 bit adc
@@ -42,14 +28,6 @@ void pointing_device_task(void) {
 #define _HPR 5
 #define _SUPER 6
 
-enum custom_keycodes {
-  BASE = SAFE_RANGE,
-  SYMBS,
-  MVMNT,
-  ADJUST,
-  BACKLIT,
-  RGBRST,
-};
 
 enum macro_keycodes {
   KC_SAMPLEMACRO,
@@ -57,12 +35,7 @@ enum macro_keycodes {
 
 #define KC______ KC_TRNS
 #define KC___  KC_NO
-#define KC_BASE BASE
-#define KC_SYMBS SYMBS
-#define KC_MVMNT MVMNT
-#define KC_MVMNT MVMNT
 #define KC_RST   RESET
-#define KC_LRST  RGBRST
 #define KC_LTOG  RGB_TOG
 #define KC_LHUI  RGB_HUI
 #define KC_LHUD  RGB_HUD
@@ -121,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_kc(
  ESC,         Q, W, E, R, T, /*|*/ Y, U,    I,   O,    P, BSPC,
  TAB,         A, S, D, F, G, /*|*/ H, J,    K,   L, SCLN, QUOT,
-      TO_LYR, Z, X, C, V, B, /*|*/ N, M, COMM, DOT, CTL_or_SLSH,  ENT,
+      TO_LYR, Z, X, C, V, B, /*|*/ N, M, COMM, DOT, SLSH,  ENT,
            BSP_MOV, LSFT, C_ESC, /*|*/ M_ENT, G_SPC, MO_SYMBS
 ),
 
@@ -154,7 +127,7 @@ TO_BASE, F1  , F2  , F3  , F4  , F5  , /*|*/ WHL , WHD , WHU ,  WHR, CLR, ENT ,
 ),
 
 [_ADJUST] = LAYOUT_kc(
-TO_BASE, LRST,  __ ,   __ , __, RGBMF, /*|*/ __, __, __, __, __, __,
+TO_BASE, __,  __ ,   __ , __, RGBMF, /*|*/ __, __, __, __, __, __,
 LTOG   , LHUI, LSAI,  LVAI, __,    __, /*|*/ __, __, __, __, __, LTOG,
 LMOD   , LHUD, LSAD,  LVAD, __,    __, /*|*/ __, __, __, __, __, __,
                 MO_MVMNT, LSFT, C_ESC, /*|*/ M_ENT, G_SPC, MO_SYMBS
@@ -168,146 +141,3 @@ TO_MOVEMENT, __, __, __, __, RST, /*|*/ RST, __, __, __, __, __,
 ),
 
 };
-
-int RGB_current_mode;
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
-
-void matrix_init_user(void) {
-    #ifdef RGBLIGHT_ENABLE
-      RGB_current_mode = rgblight_config.mode;
-    #endif
-    //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
-    #ifdef SSD1306OLED
-        iota_gfx_init(!has_usb());   // turns on the display
-    #endif
-}
-
-
-//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
-#ifdef SSD1306OLED
-
-// When add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-
-void           pointing_device_send(void);
-report_mouse_t pointing_device_get_report(void);
-void           pointing_device_set_report(report_mouse_t);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
-void matrix_scan_user(void) {
-   iota_gfx_task();
-}
-
-
-void matrix_render_user(struct CharacterMatrix *matrix) {
-  if (is_master) {
-    // If you want to change the display of OLED, you need to change here
-    matrix_write_ln(matrix, read_layer_state());
-    matrix_write_ln(matrix, read_keylog());
-    matrix_write_ln(matrix, read_keylogs());
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
-    //matrix_write_ln(matrix, read_timelog());
-  } else {
-    matrix_write(matrix, read_logo());
-  }
-}
-
-void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
-  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
-    memcpy(dest->display, source->display, sizeof(dest->display));
-    dest->dirty = true;
-  }
-}
-
-void iota_gfx_task_user(void) {
-  struct CharacterMatrix matrix;
-  matrix_clear(&matrix);
-  matrix_render_user(&matrix);
-  matrix_update(&display, &matrix);
-}
-#endif//SSD1306OLED
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef SSD1306OLED
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
-
-  //set_keylog(joystick_status.axes[0], record);
-
-  switch (keycode) {
-    case BASE:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_BASE);
-      }
-      return false;
-    case SYMBS:
-      if (record->event.pressed) {
-        layer_on(_SYMBS);
-        update_tri_layer_RGB(_SYMBS, _MVMNT, _ADJUST);
-      } else {
-        layer_off(_SYMBS);
-        update_tri_layer_RGB(_SYMBS, _MVMNT, _ADJUST);
-      }
-      return false;
-    case MVMNT:
-      if (record->event.pressed) {
-        layer_on(_MVMNT);
-        update_tri_layer_RGB(_SYMBS, _MVMNT, _ADJUST);
-      } else {
-        layer_off(_MVMNT);
-        update_tri_layer_RGB(_SYMBS, _MVMNT, _ADJUST);
-      }
-      return false;
-    case ADJUST:
-        if (record->event.pressed) {
-          layer_on(_ADJUST);
-        } else {
-          layer_off(_ADJUST);
-        }
-        return false;
-    case RGB_MOD:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          rgblight_mode(RGB_current_mode);
-          rgblight_step();
-          RGB_current_mode = rgblight_config.mode;
-        }
-      #endif
-      return false;
-    case RGBRST:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          eeconfig_update_rgblight_default();
-          rgblight_enable();
-          RGB_current_mode = rgblight_config.mode;
-        }
-      #endif
-      break;
-  }
-  return true;
-}
